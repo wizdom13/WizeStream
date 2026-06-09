589855bdf687576bef04066bd2e67bd9de5bf64e
WizDom13 <wizdom13@gmail.com>
2026-06-09 17:32:30 +0300

    Update crash report recipient email

--- app/src/main/java/org/schabi/newpipe/error/ErrorActivity.kt
+++ app/src/main/java/org/schabi/newpipe/error/ErrorActivity.kt
@@ -274,7 +274,7 @@ class ErrorActivity : AppCompatActivity() {
         // BUNDLE TAGS
         const val ERROR_INFO = "error_info"
 
-        private const val ERROR_EMAIL_ADDRESS = "crashreport@newpipe.schabi.org"
+        private const val ERROR_EMAIL_ADDRESS = "wizdom13@gmail.com"
         private const val ERROR_EMAIL_SUBJECT = "Exception in "
 
         private const val ERROR_GITHUB_ISSUE_URL = "https://github.com/TeamNewPipe/NewPipe/issues"
