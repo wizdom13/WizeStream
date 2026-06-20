dd56c40a35cbd0e9afa2eec9be0249585fc0522f
WizDom13 <wizdom13@gmail.com>
2026-06-20 14:17:50 +0300

    Set stable Gradle root project names

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+rootProject.name = "NewPipe_Material"
 
 pluginManagement {
     repositories {
