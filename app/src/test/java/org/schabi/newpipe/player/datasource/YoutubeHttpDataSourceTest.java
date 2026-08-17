6ca082527f5e5df2eac08bd70ac9d045d515b38a
WizDom13 <wizdom13@gmail.com>
2026-08-17 10:45:28 +0300

    Fix Safari YouTube stream user agent

---
+++ app/src/test/java/org/schabi/newpipe/player/datasource/YoutubeHttpDataSourceTest.java
@@ -0,0 +1,56 @@
+package org.schabi.newpipe.player.datasource;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper.getAndroidUserAgent;
+import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper.getIosUserAgent;
+import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper.getSafariUserAgent;
+import static org.schabi.newpipe.extractor.services.youtube.YoutubeParsingHelper.isSafariStreamingUrl;
+
+import org.junit.Test;
+import org.schabi.newpipe.DownloaderImpl;
+
+public class YoutubeHttpDataSourceTest {
+    private static final String STREAM_URL =
+            "https://rr1---sn.example.googlevideo.com/videoplayback?itag=18";
+
+    @Test
+    public void safariWebStreamUsesSafariUserAgent() {
+        final String url = STREAM_URL + "&c=WEB&cver=2.20260114.08.00";
+
+        assertTrue(isSafariStreamingUrl(url));
+        assertEquals(getSafariUserAgent(), YoutubeHttpDataSource.resolveUserAgent(url));
+    }
+
+    @Test
+    public void regularWebStreamUsesDefaultUserAgent() {
+        final String url = STREAM_URL + "&c=WEB&cver=2.20241126.01.00";
+
+        assertFalse(isSafariStreamingUrl(url));
+        assertEquals(DownloaderImpl.USER_AGENT, YoutubeHttpDataSource.resolveUserAgent(url));
+    }
+
+    @Test
+    public void safariVersionWithoutWebClientUsesDefaultUserAgent() {
+        final String url = STREAM_URL + "&c=TVHTML5_SIMPLY_EMBEDDED_PLAYER"
+                + "&cver=2.20260114.08.00";
+
+        assertFalse(isSafariStreamingUrl(url));
+        assertEquals(DownloaderImpl.USER_AGENT, YoutubeHttpDataSource.resolveUserAgent(url));
+    }
+
+    @Test
+    public void androidStreamKeepsAndroidUserAgent() {
+        final String url = STREAM_URL + "&c=ANDROID&cver=21.03.36";
+
+        assertEquals(getAndroidUserAgent(null), YoutubeHttpDataSource.resolveUserAgent(url));
+    }
+
+    @Test
+    public void iosStreamKeepsIosUserAgent() {
+        final String url = STREAM_URL + "&c=IOS&cver=19.45.4";
+
+        assertEquals(getIosUserAgent(null), YoutubeHttpDataSource.resolveUserAgent(url));
+    }
+}
