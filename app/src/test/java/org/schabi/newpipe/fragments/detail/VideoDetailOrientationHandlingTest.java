f8d89f60054fc2c721f47118c3408c519a8fa85f
WizDom13 <wizdom13@gmail.com>
2026-08-22 00:08:12 +0300

    Restore landscape fullscreen rotation

---
+++ app/src/test/java/org/schabi/newpipe/fragments/detail/VideoDetailOrientationHandlingTest.java
@@ -0,0 +1,27 @@
+package org.schabi.newpipe.fragments.detail;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class VideoDetailOrientationHandlingTest {
+    private final Path projectDirectory = Files.exists(Path.of("src/main"))
+            ? Path.of("src/main")
+            : Path.of("app/src/main");
+
+    @Test
+    public void handledConfigurationChangesResyncPlayerFullscreen() throws Exception {
+        final String manifest = Files.readString(projectDirectory.resolve("AndroidManifest.xml"));
+        final String fragment = Files.readString(projectDirectory.resolve(
+                "java/org/schabi/newpipe/fragments/detail/VideoDetailFragment.java"));
+
+        assertTrue(manifest.contains("android:configChanges="
+                + "\"screenSize|smallestScreenSize|screenLayout|orientation\""));
+        assertTrue(fragment.contains("public void onConfigurationChanged("));
+        assertTrue(fragment.contains("syncFullscreenWithOrientation("));
+        assertTrue(fragment.contains("binding.getRoot().post("));
+    }
+}
