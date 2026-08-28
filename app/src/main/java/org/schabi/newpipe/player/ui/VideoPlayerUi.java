fc6c7a6454af283b0583c447d9a57abb583f58ed
WizDom13 <wizdom13@gmail.com>
2026-08-28 12:35:41 +0300

    Use Material colors for seekbars

--- app/src/main/java/org/schabi/newpipe/player/ui/VideoPlayerUi.java
+++ app/src/main/java/org/schabi/newpipe/player/ui/VideoPlayerUi.java
@@ -198,14 +198,24 @@ public abstract class VideoPlayerUi extends PlayerUi implements SeekBar.OnSeekBa
     }
 
     private void applyPlayerSeekBarColor() {
-        final int playerSeekBarColor = ThemeHelper.resolveColorFromAttr(
-                binding.playbackSeekBar.getContext(), R.attr.colorPrimaryFixedDim);
-        final ColorStateList playerSeekBarColorStateList =
-                ColorStateList.valueOf(playerSeekBarColor);
+        final Context seekBarContext = binding.playbackSeekBar.getContext();
+        final ColorStateList activeColor = ColorStateList.valueOf(
+                ThemeHelper.resolveColorFromAttr(
+                        seekBarContext, R.attr.colorPrimaryFixedDim));
+        final ColorStateList bufferedColor = ColorStateList.valueOf(
+                ThemeHelper.resolveColorFromAttr(
+                        seekBarContext, R.attr.colorPrimaryContainer));
+        final ColorStateList inactiveColor = ColorStateList.valueOf(
+                ThemeHelper.resolveColorFromAttr(
+                        seekBarContext, R.attr.colorSurfaceVariant));
 
-        binding.playbackSeekBar.setProgressTintList(playerSeekBarColorStateList);
+        binding.playbackSeekBar.setProgressTintList(activeColor);
         binding.playbackSeekBar.setProgressTintMode(PorterDuff.Mode.SRC_IN);
-        binding.playbackSeekBar.setThumbTintList(playerSeekBarColorStateList);
+        binding.playbackSeekBar.setSecondaryProgressTintList(bufferedColor);
+        binding.playbackSeekBar.setSecondaryProgressTintMode(PorterDuff.Mode.SRC_IN);
+        binding.playbackSeekBar.setProgressBackgroundTintList(inactiveColor);
+        binding.playbackSeekBar.setProgressBackgroundTintMode(PorterDuff.Mode.SRC_IN);
+        binding.playbackSeekBar.setThumbTintList(activeColor);
         binding.playbackSeekBar.setThumbTintMode(PorterDuff.Mode.SRC_IN);
     }
 
