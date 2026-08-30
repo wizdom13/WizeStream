1f5840ab9e45651b4fc14acd4369049d735164f4
WizDom13 <wizdom13@gmail.com>
2026-08-30 21:53:00 +0300

    Add configurable tablet grid columns

--- app/src/test/java/org/schabi/newpipe/util/GridLayoutManagerHelperTest.java
+++ app/src/test/java/org/schabi/newpipe/util/GridLayoutManagerHelperTest.java
@@ -12,6 +12,32 @@ public class GridLayoutManagerHelperTest {
     }
 
     @Test
+    public void configuredColumnsOverrideAutomaticSizing() {
+        assertEquals(2, GridLayoutManagerHelper.calculateSpanCount(1024, 188, 2));
+        assertEquals(3, GridLayoutManagerHelper.calculateSpanCount(1024, 188, 3));
+        assertEquals(4, GridLayoutManagerHelper.calculateSpanCount(1024, 188, 4));
+    }
+
+    @Test
+    public void invalidConfiguredColumnsFallBackToAutomaticSizing() {
+        assertEquals(5, GridLayoutManagerHelper.calculateSpanCount(1024, 188, 0));
+        assertEquals(5, GridLayoutManagerHelper.calculateSpanCount(1024, 188, 1));
+        assertEquals(5, GridLayoutManagerHelper.calculateSpanCount(1024, 188, 5));
+    }
+
+    @Test
+    public void preferenceValuesAreParsedAndValidated() {
+        assertEquals(0, GridLayoutManagerHelper.parsePreferredSpanCount(null));
+        assertEquals(0, GridLayoutManagerHelper.parsePreferredSpanCount("auto"));
+        assertEquals(0, GridLayoutManagerHelper.parsePreferredSpanCount("invalid"));
+        assertEquals(0, GridLayoutManagerHelper.parsePreferredSpanCount("1"));
+        assertEquals(0, GridLayoutManagerHelper.parsePreferredSpanCount("5"));
+        assertEquals(2, GridLayoutManagerHelper.parsePreferredSpanCount("2"));
+        assertEquals(3, GridLayoutManagerHelper.parsePreferredSpanCount("3"));
+        assertEquals(4, GridLayoutManagerHelper.parsePreferredSpanCount("4"));
+    }
+
+    @Test
     public void narrowAndInvalidMeasurementsKeepOneColumn() {
         assertEquals(1, GridLayoutManagerHelper.calculateSpanCount(180, 240));
         assertEquals(1, GridLayoutManagerHelper.calculateSpanCount(0, 240));
