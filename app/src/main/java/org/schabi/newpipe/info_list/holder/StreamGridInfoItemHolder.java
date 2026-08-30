b84e190cc45306c33ab7c24fe2e23507b3cf26b6
WizDom13 <wizdom13@gmail.com>
2026-08-30 21:44:36 +0300

    Show full video titles in grid view

--- app/src/main/java/org/schabi/newpipe/info_list/holder/StreamGridInfoItemHolder.java
+++ app/src/main/java/org/schabi/newpipe/info_list/holder/StreamGridInfoItemHolder.java
@@ -3,10 +3,20 @@ package org.schabi.newpipe.info_list.holder;
 import android.view.ViewGroup;
 
 import org.schabi.newpipe.R;
+import org.schabi.newpipe.extractor.InfoItem;
 import org.schabi.newpipe.info_list.InfoItemBuilder;
+import org.schabi.newpipe.local.history.HistoryRecordManager;
+import org.schabi.newpipe.util.GridTitleDisplayPolicy;
 
 public class StreamGridInfoItemHolder extends StreamInfoItemHolder {
     public StreamGridInfoItemHolder(final InfoItemBuilder infoItemBuilder, final ViewGroup parent) {
         super(infoItemBuilder, R.layout.list_stream_grid_item, parent);
+    }
+
+    @Override
+    public void updateFromItem(final InfoItem infoItem,
+                               final HistoryRecordManager historyRecordManager) {
+        GridTitleDisplayPolicy.apply(itemVideoTitleView);
+        super.updateFromItem(infoItem, historyRecordManager);
     }
 }
--- app/src/main/java/org/schabi/newpipe/local/holder/LocalStatisticStreamGridItemHolder.java
+++ app/src/main/java/org/schabi/newpipe/local/holder/LocalStatisticStreamGridItemHolder.java
@@ -3,11 +3,24 @@ package org.schabi.newpipe.local.holder;
 import android.view.ViewGroup;
 
 import org.schabi.newpipe.R;
+import org.schabi.newpipe.database.LocalItem;
 import org.schabi.newpipe.local.LocalItemBuilder;
+import org.schabi.newpipe.local.history.HistoryRecordManager;
+import org.schabi.newpipe.util.GridTitleDisplayPolicy;
+
+import java.time.format.DateTimeFormatter;
 
 public class LocalStatisticStreamGridItemHolder extends LocalStatisticStreamItemHolder {
     public LocalStatisticStreamGridItemHolder(final LocalItemBuilder infoItemBuilder,
                                               final ViewGroup parent) {
         super(infoItemBuilder, R.layout.list_stream_grid_item, parent);
+    }
+
+    @Override
+    public void updateFromItem(final LocalItem localItem,
+                               final HistoryRecordManager historyRecordManager,
+                               final DateTimeFormatter dateTimeFormatter) {
+        GridTitleDisplayPolicy.apply(itemVideoTitleView);
+        super.updateFromItem(localItem, historyRecordManager, dateTimeFormatter);
     }
 }
