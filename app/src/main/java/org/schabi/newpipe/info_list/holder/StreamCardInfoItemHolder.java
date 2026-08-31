592ca600429d0d1ffb911be81d5ea1ab4ad6c76e
WizDom13 <wizdom13@gmail.com>
2026-08-31 11:15:24 +0300

    Apply full-title policy to stream cards

--- app/src/main/java/org/schabi/newpipe/info_list/holder/StreamCardInfoItemHolder.java
+++ app/src/main/java/org/schabi/newpipe/info_list/holder/StreamCardInfoItemHolder.java
@@ -3,7 +3,10 @@ package org.schabi.newpipe.info_list.holder;
 import android.view.ViewGroup;
 
 import org.schabi.newpipe.R;
+import org.schabi.newpipe.extractor.InfoItem;
 import org.schabi.newpipe.info_list.InfoItemBuilder;
+import org.schabi.newpipe.local.history.HistoryRecordManager;
+import org.schabi.newpipe.util.GridTitleDisplayPolicy;
 
 /**
  * Card layout for stream.
@@ -12,5 +15,12 @@ public class StreamCardInfoItemHolder extends StreamInfoItemHolder {
 
     public StreamCardInfoItemHolder(final InfoItemBuilder infoItemBuilder, final ViewGroup parent) {
         super(infoItemBuilder, R.layout.list_stream_card_item, parent);
+    }
+
+    @Override
+    public void updateFromItem(final InfoItem infoItem,
+                               final HistoryRecordManager historyRecordManager) {
+        GridTitleDisplayPolicy.apply(itemVideoTitleView);
+        super.updateFromItem(infoItem, historyRecordManager);
     }
 }
