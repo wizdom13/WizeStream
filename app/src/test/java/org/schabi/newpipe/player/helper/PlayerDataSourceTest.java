42f0abfa94c1142372430308765e65b92c9f28a9
WizDom13 <wizdom13@gmail.com>
2026-08-31 22:45:41 +0300

    Cover BiliBili playback transport routing

--- app/src/test/java/org/schabi/newpipe/player/helper/PlayerDataSourceTest.java
+++ app/src/test/java/org/schabi/newpipe/player/helper/PlayerDataSourceTest.java
@@ -1,11 +1,14 @@
 package org.schabi.newpipe.player.helper;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.Test;
 import org.schabi.newpipe.extractor.services.youtube.dashmanifestcreators.YoutubeOtfDashManifestCreator;
 import org.schabi.newpipe.extractor.services.youtube.dashmanifestcreators.YoutubePostLiveStreamDvrDashManifestCreator;
 import org.schabi.newpipe.extractor.services.youtube.dashmanifestcreators.YoutubeProgressiveDashManifestCreator;
+
+import java.util.Map;
 
 public class PlayerDataSourceTest {
     @Test
@@ -19,5 +22,14 @@ public class PlayerDataSourceTest {
         assertEquals(0, YoutubeProgressiveDashManifestCreator.getCache().size());
         assertEquals(0, YoutubeOtfDashManifestCreator.getCache().size());
         assertEquals(0, YoutubePostLiveStreamDvrDashManifestCreator.getCache().size());
+    }
+
+    @Test
+    public void bilibiliPlaybackUsesCanonicalAntiLeechHeadersWithoutCookies() {
+        final Map<String, String> headers = PlayerDataSource.getBilibiliPlaybackHeaders();
+
+        assertEquals("https://www.bilibili.com/", headers.get("Referer"));
+        assertEquals("zh-CN,zh;q=0.9", headers.get("Accept-Language"));
+        assertFalse(headers.containsKey("Cookie"));
     }
 }
