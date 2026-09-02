4afe63fa1c7299a7768fa137ff0b5a8502145502
WizDom13 <wizdom13@gmail.com>
2026-09-02 08:07:02 +0300

    Prevent crashes on devices without picture-in-picture

--- app/src/test/java/org/schabi/newpipe/player/pip/NativePipControllerTest.java
+++ app/src/test/java/org/schabi/newpipe/player/pip/NativePipControllerTest.java
@@ -1,6 +1,10 @@
 package org.schabi.newpipe.player.pip;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import android.os.Build;
 
 import org.junit.Test;
 
@@ -23,5 +27,21 @@ public class NativePipControllerTest {
     public void normalVideoAspectRatiosArePreserved() {
         assertEquals(16f / 9f, NativePipController.sanitizeAspectRatio(16f / 9f), TOLERANCE);
         assertEquals(9f / 16f, NativePipController.sanitizeAspectRatio(9f / 16f), TOLERANCE);
+    }
+
+    @Test
+    public void pipRequiresTheSystemFeatureOnSupportedAndroidVersions() {
+        assertFalse(NativePipController.isSupportedEnvironment(
+                Build.VERSION_CODES.O, false, false, false));
+        assertTrue(NativePipController.isSupportedEnvironment(
+                Build.VERSION_CODES.O, true, false, false));
+    }
+
+    @Test
+    public void pipRemainsDisabledOnTvAndInMultiWindowMode() {
+        assertFalse(NativePipController.isSupportedEnvironment(
+                Build.VERSION_CODES.O, true, true, false));
+        assertFalse(NativePipController.isSupportedEnvironment(
+                Build.VERSION_CODES.O, true, false, true));
     }
 }
