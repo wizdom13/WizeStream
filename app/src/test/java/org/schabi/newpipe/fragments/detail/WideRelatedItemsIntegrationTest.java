fad27a6b5fba224317476bd72541d15fb4eec718
WizDom13 <wizdom13@gmail.com>
2026-09-04 10:28:06 +0300

    Honor full titles in wide related videos

--- app/src/test/java/org/schabi/newpipe/fragments/detail/WideRelatedItemsIntegrationTest.java
+++ app/src/test/java/org/schabi/newpipe/fragments/detail/WideRelatedItemsIntegrationTest.java
@@ -19,11 +19,16 @@ public class WideRelatedItemsIntegrationTest {
         final String relatedItems = read("org/schabi/newpipe/fragments/list/videos/"
                 + "RelatedItemsFragment.java");
         final String adapter = read("org/schabi/newpipe/info_list/InfoListAdapter.java");
+        final String wideRelatedHolder = read("org/schabi/newpipe/info_list/holder/"
+                + "StreamWideRelatedInfoItemHolder.java");
 
         assertTrue(videoDetail.contains("RelatedItemsFragment.getInstance(info, true)"));
         assertTrue(videoDetail.contains("RelatedItemsFragment.getInstance(info)"));
         assertTrue(relatedItems.contains("setUseWideRelatedVariant("));
-        assertTrue(adapter.contains("R.layout.list_stream_related_wide_item"));
+        assertTrue(adapter.contains("case WIDE_RELATED_STREAM_HOLDER_TYPE:"));
+        assertTrue(adapter.contains(
+                "return new StreamWideRelatedInfoItemHolder(infoItemBuilder, parent);"));
+        assertTrue(wideRelatedHolder.contains("R.layout.list_stream_related_wide_item"));
     }
 
     private String read(final String relativePath) throws Exception {
