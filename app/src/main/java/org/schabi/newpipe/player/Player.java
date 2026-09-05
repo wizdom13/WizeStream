26603c05d979da07bbee9f5f41eae915d2deb1ce
WizDom13 <wizdom13@gmail.com>
2026-09-05 15:44:28 +0300

    Expose queue update bridge

--- app/src/main/java/org/schabi/newpipe/player/Player.java
+++ app/src/main/java/org/schabi/newpipe/player/Player.java
@@ -1117,7 +1117,7 @@ public final class Player implements PlaybackListener, Listener {
         eventDispatcher.stopBindings();
     }
 
-    private void notifyQueueUpdateToListeners() {
+    void notifyQueueUpdateToListeners() {
         eventDispatcher.notifyQueueUpdate();
     }
 
