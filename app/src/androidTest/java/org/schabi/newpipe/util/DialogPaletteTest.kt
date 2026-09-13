00ee95d886b3763bab80f985866682ce2eaec7a7
WizDom13 <wizdom13@gmail.com>
2026-09-13 18:39:27 +0300

    Match the project import ordering

--- app/src/androidTest/java/org/schabi/newpipe/util/DialogPaletteTest.kt
+++ app/src/androidTest/java/org/schabi/newpipe/util/DialogPaletteTest.kt
@@ -16,6 +16,7 @@ import androidx.preference.PreferenceManager
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.android.material.R as MaterialR
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.junit.After
 import org.junit.Assert.assertEquals
@@ -24,7 +25,6 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.schabi.newpipe.R
-import com.google.android.material.R as MaterialR
 
 @RunWith(AndroidJUnit4::class)
 class DialogPaletteTest {
