241b57e85cb75b1eb4851ab3492c7b994fdfc5b9
WizDom13 <wizdom13@gmail.com>
2026-09-18 23:53:05 +0300

    Use current schema constant in backup validation

--- app/src/androidTest/java/org/schabi/newpipe/settings/BackupDatabaseValidationTest.kt
+++ app/src/androidTest/java/org/schabi/newpipe/settings/BackupDatabaseValidationTest.kt
@@ -106,7 +106,7 @@ class BackupDatabaseValidationTest {
             null
         ).use { database ->
             database.execSQL("CREATE TABLE foreign_data (id INTEGER PRIMARY KEY)")
-            database.version = Migrations.DB_VER_24
+            database.version = Migrations.DB_VER_CURRENT
         }
 
         try {
