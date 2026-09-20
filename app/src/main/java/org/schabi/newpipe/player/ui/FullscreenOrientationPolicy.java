0cd930f4b5f46a12050e2a4535a3ac8928f7b136
WizDom13 <wizdom13@gmail.com>
2026-09-20 14:25:35 +0300

    Lock manual portrait fullscreen orientation

--- app/src/main/java/org/schabi/newpipe/player/ui/FullscreenOrientationPolicy.java
+++ app/src/main/java/org/schabi/newpipe/player/ui/FullscreenOrientationPolicy.java
@@ -59,11 +59,19 @@ public final class FullscreenOrientationPolicy {
 
     public static boolean shouldUseOrientationAction(
             final VideoContentOrientation contentOrientation,
-            final boolean landscape) {
+            final boolean landscape,
+            final boolean targetFullscreen) {
         if (contentOrientation == VideoContentOrientation.LANDSCAPE) {
             return true;
         }
-        return contentOrientation == VideoContentOrientation.PORTRAIT && landscape;
+        return contentOrientation == VideoContentOrientation.PORTRAIT
+                && (targetFullscreen || landscape);
+    }
+
+    public static boolean shouldLockPortraitFullscreen(
+            final boolean fullscreen,
+            final VideoContentOrientation contentOrientation) {
+        return fullscreen && contentOrientation == VideoContentOrientation.PORTRAIT;
     }
 
     public static int targetConfigurationOrientation(
