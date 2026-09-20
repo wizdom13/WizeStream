8732355c81db503b99fbebfa66eb4073f9b0d364
WizDom13 <wizdom13@gmail.com>
2026-09-20 23:43:05 +0300

    Fix profile policy test imports

--- app/src/test/java/org/schabi/newpipe/profiles/ProfilePolicyTest.kt
+++ app/src/test/java/org/schabi/newpipe/profiles/ProfilePolicyTest.kt
@@ -1,8 +1,8 @@
 package org.schabi.newpipe.profiles
 
-import static org.junit.Assert.assertEquals
-import static org.junit.Assert.assertFalse
-import static org.junit.Assert.assertTrue
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 
 import org.junit.Test
 
