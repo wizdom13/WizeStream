4e7a30f2b1845319ff36238e1db19430a2ef150e
WizDom13 <wizdom13@gmail.com>
2026-09-21 15:39:18 +0300

    Scope feed group selector by profile

--- app/src/main/java/org/schabi/newpipe/settings/SelectFeedGroupFragment.java
+++ app/src/main/java/org/schabi/newpipe/settings/SelectFeedGroupFragment.java
@@ -25,6 +25,7 @@ import org.schabi.newpipe.R;
 import org.schabi.newpipe.database.AppDatabase;
 import org.schabi.newpipe.database.feed.model.FeedGroupEntity;
 import org.schabi.newpipe.error.ErrorUtil;
+import org.schabi.newpipe.profiles.ProfileManager;
 import org.schabi.newpipe.util.ThemeHelper;
 
 import java.util.List;
@@ -81,12 +82,21 @@ public class SelectFeedGroupFragment extends DialogFragment {
 
 
         final AppDatabase database = NewPipeDatabase.getInstance(requireContext());
-        database.feedGroupDAO().getAll().toObservable()
+        feedGroupsForProfile(
+                database,
+                ProfileManager.getActiveProfileId(requireContext()))
+                .toObservable()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(getFeedGroupObserver());
 
         return v;
+    }
+
+    static io.reactivex.rxjava3.core.Flowable<List<FeedGroupEntity>> feedGroupsForProfile(
+            final AppDatabase database,
+            final String profileId) {
+        return database.feedGroupDAO().getAllForProfile(profileId);
     }
 
     /*//////////////////////////////////////////////////////////////////////////
