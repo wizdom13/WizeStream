9e9fd0520956675acd1deaf44f382d0014ce3fa1
WizDom13 <wizdom13@gmail.com>
2026-09-23 20:14:28 +0300

    Select feed refresh indicator style

--- app/src/main/java/org/schabi/newpipe/local/feed/FeedFragment.kt
+++ app/src/main/java/org/schabi/newpipe/local/feed/FeedFragment.kt
@@ -511,24 +511,46 @@ class FeedFragment : BaseStateFragment<FeedState>(), ContextualSearchable {
 
         val isIndeterminate = progressState.currentProgress < 0 ||
             progressState.maxProgress < 0
-        feedBinding.loadingProgressBar.isVisible = !isIndeterminate
-        feedBinding.loadingIndeterminateProgressBar.isVisible = isIndeterminate
+        val preferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        val indicatorStyle = preferences.getString(
+            getString(R.string.feed_refresh_indicator_style_key),
+            getString(R.string.feed_refresh_indicator_style_wavy_value)
+        )
+        val useWavyIndicator =
+            indicatorStyle != getString(R.string.feed_refresh_indicator_style_line_value)
+
+        feedBinding.loadingWavyProgressBar.isVisible = useWavyIndicator
+        feedBinding.loadingProgressBar.isVisible = !useWavyIndicator && !isIndeterminate
+        feedBinding.loadingIndeterminateProgressBar.isVisible =
+            !useWavyIndicator && isIndeterminate
+        feedBinding.loadingWavyProgressBar.isIndeterminate =
+            useWavyIndicator && isIndeterminate
 
         if (isIndeterminate) {
-            feedBinding.loadingIndeterminateProgressBar.contentDescription =
-                if (progressState.progressMessage > 0) {
-                    getString(progressState.progressMessage)
-                } else {
-                    getString(R.string.feed_notification_loading)
-                }
+            val description = if (progressState.progressMessage > 0) {
+                getString(progressState.progressMessage)
+            } else {
+                getString(R.string.feed_notification_loading)
+            }
+            if (useWavyIndicator) {
+                feedBinding.loadingWavyProgressBar.contentDescription = description
+            } else {
+                feedBinding.loadingIndeterminateProgressBar.contentDescription = description
+            }
         } else {
             val maxProgress = progressState.maxProgress.coerceAtLeast(1)
             val currentProgress = progressState.currentProgress.coerceIn(0, maxProgress)
-            feedBinding.loadingProgressBar.max = maxProgress
-            feedBinding.loadingProgressBar.setProgressCompat(currentProgress, true)
-            feedBinding.loadingProgressBar.contentDescription =
-                getString(R.string.feed_notification_loading) + " " +
+            val description = getString(R.string.feed_notification_loading) + " " +
                 currentProgress + "/" + maxProgress
+            if (useWavyIndicator) {
+                feedBinding.loadingWavyProgressBar.max = maxProgress
+                feedBinding.loadingWavyProgressBar.setProgressCompat(currentProgress, true)
+                feedBinding.loadingWavyProgressBar.contentDescription = description
+            } else {
+                feedBinding.loadingProgressBar.max = maxProgress
+                feedBinding.loadingProgressBar.setProgressCompat(currentProgress, true)
+                feedBinding.loadingProgressBar.contentDescription = description
+            }
         }
     }
 
